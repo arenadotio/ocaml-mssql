de20670aca8dfdd7c016fb57a1345739bab38750
Brendan Long <self@brendanlong.com>
2018-11-16 13:39:29 -0500

    Revert "Switch from Dblib.canquery to Dblib.cancel"
    This reverts commit 015d7724d631ea41fa0f0db5ba6b9c54e900e48f.

--- src/client.ml
+++ src/client.ml
@@ -54,7 +54,7 @@ let run_query ~month_offset t query =
     |> List.range 0
     |> List.map ~f:(fun i -> Dblib.colname t (i + 1))
   in
-  Dblib.cancel t;
+  Dblib.canquery t;
   Dblib.sqlexec t query;
   let rec result_set_loop result_sets =
     match Dblib.results t with
