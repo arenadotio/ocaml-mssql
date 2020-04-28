b7e2bd3607c9120d11f34aeaaab95681f4b9599d
Brendan Long <self@brendanlong.com>
2020-04-28 09:57:34 -0600

    Move async context handling into Async_helper

--- src/async_helper.ml
+++ src/async_helper.ml
@@ -1,8 +1,22 @@
+open Core
 open Async
 
 (* Run a function in async without the caller needing to keep track.
    If we're already in Async's main thread, just run it, otherwise
    do cross-thread magic *)
-let safely_run_in_async f =
-  if Thread_safe.am_holding_async_lock () then f () else Thread_safe.run_in_async_exn f
+let safely_run_in_async ?context f =
+  let in_context =
+    match context with
+    | Some context ->
+      (* Once we're on Async v0.13, use Scheduler.enqueue, since that will apparently pass exceptions
+        through correctly *)
+      fun () ->
+       Scheduler.within_context context f
+       |> Result.ok
+       |> Option.value_exn ~here:[%here] ~message:"Unknown exception in logging"
+    | None -> f
+  in
+  if Thread_safe.am_holding_async_lock ()
+  then in_context ()
+  else Thread_safe.run_in_async_exn in_context
 ;;
--- src/logger.ml
+++ src/logger.ml
@@ -1,23 +1,10 @@
 open Core
-open Async
 
 let src = Logs.Src.create "mssql"
 let lib_tag = Logs.Tag.def "lib" Format.pp_print_string
 
 let msg ?(tags = Logs.Tag.empty) ?context level fmt =
-  Async_helper.safely_run_in_async
-  @@ fun () ->
-  let in_context f =
-    match context with
-    | Some context ->
-      (* Once we're on Async v0.13, use Scheduler.enqueue, since that will apparently pass exceptions
-        through correctly *)
-      Scheduler.within_context context f
-      |> Result.ok
-      |> Option.value_exn ~here:[%here] ~message:"Unknown exception in logging"
-    | None -> f ()
-  in
-  in_context
+  Async_helper.safely_run_in_async ?context
   @@ fun () ->
   ksprintf
     (fun msg ->
